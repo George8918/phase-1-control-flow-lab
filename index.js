1f0914b52c7de33236305e4348bf3dde9b4267f8
George <GZheng8918@gmail.com>
2023-03-16 22:48:46 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+let greeting;
+
+  if (ride <= 400) {
+    greeting= 'This one is on me!';
+    
+  } else if (400<=ride && ride <= 2000){
+    greeting= 'That will be twenty bucks.';
+    
+  } else if (ride > 2500) {
+    greeting = 'No can do.';
+    
+  } else {
+    greeting =  'I will gladly take your thirty bucks.';
+  
+  }
+  return greeting;
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(cityName){
   // Write your code here!
+const message = cityName === "NYC" ? "Ok, sounds good." : "No go.";
+return message;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let tipGreeting;
+
+  switch(tip) {
+  case "generous":
+    tipGreeting = "Thank you so much.";
+    break;
+  
+  case "not as generous":
+    tipGreeting = "Thank you.";
+    break;
+
+  default:
+    tipGreeting = 'Bye.';
+    break;
+
+  }
+  return tipGreeting;
 }
